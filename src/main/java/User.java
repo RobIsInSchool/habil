72d563337fd7a2ba97b2bc45f4d438982604809c
RobIsInSchool <radams8@madisoncollege.edu>
2021-02-11 11:37:31 -0600

    added logging to User class, modified log file name, added logs directory

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,4 +2,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class User {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    public void testLogging() {
+        logger.info("Logging works");
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.testLogging();
+    }
 }
