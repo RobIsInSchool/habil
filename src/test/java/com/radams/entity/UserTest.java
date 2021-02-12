e8cb41fbf54529a36c63e5fd291cc91da28ba588
RobIsInSchool <radams8@madisoncollege.edu>
2021-02-12 10:01:30 -0600

    setting up tests for User

--- src/test/java/com/radams/entity/UserTest.java
+++ src/test/java/com/radams/entity/UserTest.java
@@ -1,4 +1,16 @@
 package com.radams.entity;
 
+import com.radams.entity.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class UserTest {
+
+    @Before
+    public void setUp() {
+        User user = new User();
+    }
 }
