c8c0cacc4f4d5cabf7f9c765f1ea909c09f3d654
RobIsInSchool <radams8@madisoncollege.edu>
2021-02-12 12:59:45 -0600

    added empty method to dao tests, added main method to UserDao

--- src/main/java/com/radams/persistence/UserDao.java
+++ src/main/java/com/radams/persistence/UserDao.java
@@ -23,6 +23,12 @@ public class UserDao {
         Root<User> root = query.from(User.class);
         List<User> users = session.createQuery(query).getResultList();
         session.close();
+        logger.info(users);
         return users;
+    }
+
+    public static void main(String[] args) {
+        UserDao dao = new UserDao();
+        dao.getAllUsers();
     }
 }
