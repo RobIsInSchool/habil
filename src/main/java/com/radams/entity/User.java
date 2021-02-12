f2cdacc90279ef8313cc7278902d5f6dd1e9c0cc
RobIsInSchool <radams8@madisoncollege.edu>
2021-02-12 11:32:22 -0600

    added annotations to User class

--- src/main/java/com/radams/entity/User.java
+++ src/main/java/com/radams/entity/User.java
@@ -2,20 +2,55 @@ package com.radams.entity;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+
+@Entity(name="User")
+@Table(name="users")
 public class User {
     private final Logger logger = LogManager.getLogger(this.getClass());
 
+    @Id
+    @GeneratedValue(Strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name = "user_id")
     private int userId;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "lessons_taken")
     private int lessonsTaken;
+
+    @Column(name = "lessons_taught")
     private int lessonsTaught;
+
+    @Column(name = "is_active")
     private boolean isActive;
-    private int role_id;
+
+    @Column(name = "role_id")
+    private int roleId;
+
+    @Column(name = "date_created")
+    private Date dateCreated;
+
+    @Column(name = "date_deleted")
+    private Date dateDeleted;
 
     public User() {
 
@@ -93,18 +128,35 @@ public class User {
         isActive = active;
     }
 
-    public int getRole_id() {
-        return role_id;
+    public int getRoleId() {
+        return roleId;
     }
 
-    public void setRole_id(int role_id) {
-        this.role_id = role_id;
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateDeleted() {
+        return dateDeleted;
+    }
+
+    public void setDateDeleted(Date dateDeleted) {
+        this.dateDeleted = dateDeleted;
     }
 
     @Override
     public String toString() {
         return "User{" +
-                "userId=" + userId +
+                "logger=" + logger +
+                ", userId=" + userId +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", username='" + username + '\'' +
@@ -113,7 +165,9 @@ public class User {
                 ", lessonsTaken=" + lessonsTaken +
                 ", lessonsTaught=" + lessonsTaught +
                 ", isActive=" + isActive +
-                ", role_id=" + role_id +
+                ", roleId=" + roleId +
+                ", dateCreated=" + dateCreated +
+                ", dateDeleted=" + dateDeleted +
                 '}';
     }
 }
