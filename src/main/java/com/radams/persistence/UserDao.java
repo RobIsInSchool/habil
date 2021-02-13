a38653d76359dd388004c24b4be93c00ee78356a
RobIsInSchool <radams8@madisoncollege.edu>
2021-02-13 11:21:37 -0600

    added additional CRUD methods to UserDao

--- src/main/java/com/radams/persistence/UserDao.java
+++ src/main/java/com/radams/persistence/UserDao.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -12,11 +13,19 @@ import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
+/**
+ * This is the Database Access Object for the User class
+ * @author Robert Adams
+ */
 public class UserDao {
     private final Logger logger = LogManager.getLogger(this.getClass());
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
 
+    /**
+     * Gets all users and returns them in a list
+     * @return list of results
+     */
     public List<User> getAllUsers() {
         Session session = sessionFactory.openSession();
         CriteriaBuilder builder = session.getCriteriaBuilder();
@@ -28,6 +37,11 @@ public class UserDao {
         return users;
     }
 
+    /**
+     * gets a users by a specified last name and puts them into a list
+     * @param lastName last name to search
+     * @return list of results
+     */
     public List<User> getUsersByLastName(String lastName) {
         Session session = sessionFactory.openSession();
         CriteriaBuilder builder = session.getCriteriaBuilder();
@@ -41,11 +55,52 @@ public class UserDao {
         return users;
     }
 
+    /**
+     * gets specific user by id
+     * @param id id of searched user
+     * @return user with specified id
+     */
     public User getUserById(int id) {
         Session session = sessionFactory.openSession();
         User user = session.get(User.class, id);
         session.close();
         return user;
+    }
+
+    /**
+     * update user
+     * @param user  User to be inserted or updated
+     */
+    public void saveOrUpdate(User user) {
+        Session session = sessionFactory.openSession();
+        session.saveOrUpdate(user);
+        session.close();
+    }
+
+    /**
+     * update user
+     * @param user  User to be inserted or updated
+     */
+    public int insert(User user) {
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        id = (int)session.save(user);
+        transaction.commit();
+        session.close();
+        return id;
+    }
+
+    /**
+     * Delete a user
+     * @param user User to be deleted
+     */
+    public void delete(User user) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        transaction.commit();
+        session.close();
     }
 
 }
