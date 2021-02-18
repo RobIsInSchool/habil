b38fd7cac7e4d2ca259de9fa71a36841b7cdceb4
RobIsInSchool <radams8@madisoncollege.edu>
2021-02-18 07:43:58 -0600

    added UserRole class

---
+++ src/main/java/com/radams/entity/UserRole.java
@@ -0,0 +1,52 @@
+package com.radams.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity(name="UserRole")
+@Table(name="user_roles")
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name="role_id")
+    private int roleId;
+
+    @Column(name="role_name")
+    private String roleName;
+
+    public UserRole() {
+
+    }
+
+    public UserRole(int roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "roleId=" + roleId +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
+}
