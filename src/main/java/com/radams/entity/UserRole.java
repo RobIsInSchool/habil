5ef89f3041a39f0c1caac195429f74c90dd49463
RobIsInSchool <radams8@madisoncollege.edu>
2021-02-24 12:04:47 -0600

    new and improved user role class

--- src/main/java/com/radams/entity/UserRole.java
+++ src/main/java/com/radams/entity/UserRole.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 
 /**
  * This class represents a role that a Habil user will have
+ *
  * @author Robert Adams
  */
 @Entity(name="UserRole")
@@ -18,9 +19,89 @@ public class UserRole {
     @Column(name="id")
     private int roleId;
 
-    @Column(name = "user_name")
-    private String userName;
-
     @Column(name = "role_name")
     private String roleName;
+
+    private User user;
+
+    /**
+     * Instantiates a new User role.
+     */
+    public UserRole() {
+    }
+
+    /**
+     * Instantiates a new User role.
+     *
+     * @param roleName the role name
+     * @param user     the user
+     */
+    public UserRole(String roleName, User user) {
+        this.roleName = roleName;
+        this.user = user;
+    }
+
+    /**
+     * Gets role id.
+     *
+     * @return the role id
+     */
+    public int getRoleId() {
+        return roleId;
+    }
+
+    /**
+     * Sets role id.
+     *
+     * @param roleId the role id
+     */
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    /**
+     * Gets role name.
+     *
+     * @return the role name
+     */
+    public String getRoleName() {
+        return roleName;
+    }
+
+    /**
+     * Sets role name.
+     *
+     * @param roleName the role name
+     */
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    /**
+     * Gets user.
+     *
+     * @return the user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Sets user.
+     *
+     * @param user the user
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "roleId=" + roleId +
+                ", roleName='" + roleName + '\'' +
+                ", user=" + user +
+                '}';
+    }
+
 }
