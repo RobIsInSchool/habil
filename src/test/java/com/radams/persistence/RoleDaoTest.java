34ed69823813ab7e2e0a979b54291d3d70f800b4
RobIsInSchool <radams8@madisoncollege.edu>
2021-02-27 10:52:38 -0600

    refactor of genericDao to userRoleDao, test user now comes from own method to reduce code

--- src/test/java/com/radams/persistence/RoleDaoTest.java
+++ src/test/java/com/radams/persistence/RoleDaoTest.java
@@ -3,38 +3,41 @@ package com.radams.persistence;
 import com.radams.entity.User;
 import com.radams.entity.UserRole;
 import com.radams.test.util.Database;
+import net.bytebuddy.description.type.TypeList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoleDaoTest {
 
-    UserRoleDao dao;
-    GenericDao genericDao;
+    GenericDao userRoleDao;
 
     @BeforeEach
     void setUp() {
-        genericDao = new GenericDao(UserRole.class);
-        dao = new UserRoleDao();
+        userRoleDao = new GenericDao(UserRole.class);
         Database db = Database.getInstance();
         db.runSQL("cleandb.sql");
     }
 
+    private User getTestUserWithIdOf1() {
+        GenericDao<User> userDao = new GenericDao<>(User.class);
+        User testUser = userDao.getById(1);
+        return testUser;
+    }
+
     @Test
     void getAllUserRolesSuccess() {
-        List<UserRole> userRoles = genericDao.getAll();
+        List<UserRole> userRoles = userRoleDao.getAll();
         assertEquals(6, userRoles.size());
     }
 
 
     @Test
     void getByIdSuccess() {
-        UserRole retrievedUserRole = (UserRole)genericDao.getById(3);
+        UserRole retrievedUserRole = (UserRole) userRoleDao.getById(3);
         assertNotNull(retrievedUserRole);
         assertEquals("admin", retrievedUserRole.getRoleName());
     }
@@ -42,26 +45,24 @@ class RoleDaoTest {
     @Test
     void saveOrUpdateSuccess() {
         String newRoleName = "changedRole";
-        GenericDao userDao = new GenericDao(User.class);
-        User testUser = (User)userDao.getById(1);
+        User testUser = getTestUserWithIdOf1();
         UserRole testUserRole = new UserRole("unitTestser", testUser);
         testUser.addRole(testUserRole);
-        int newId = genericDao.insert(testUserRole);
-        UserRole userRoleToUpdate = (UserRole)genericDao.getById(newId);
+        int newId = userRoleDao.insert(testUserRole);
+        UserRole userRoleToUpdate = (UserRole) userRoleDao.getById(newId);
         userRoleToUpdate.setRoleName(newRoleName);
-        genericDao.saveOrUpdate(userRoleToUpdate);
-        UserRole retrievedUserRole = (UserRole)genericDao.getById(newId);
+        userRoleDao.saveOrUpdate(userRoleToUpdate);
+        UserRole retrievedUserRole = (UserRole) userRoleDao.getById(newId);
         assertEquals(newRoleName, retrievedUserRole.getRoleName());
     }
 
     @Test
     void insertSuccess() {
-        GenericDao userDao = new GenericDao(User.class);
-        User testUser = (User)userDao.getById(1);
+        User testUser = getTestUserWithIdOf1();
         UserRole testUserRole = new UserRole("unitTester", testUser);
         testUser.addRole(testUserRole);
-        int newId = genericDao.insert(testUserRole);
-        UserRole retrievedUserRole = (UserRole)genericDao.getById(newId);
+        int newId = userRoleDao.insert(testUserRole);
+        UserRole retrievedUserRole = (UserRole) userRoleDao.getById(newId);
         assertEquals("unitTester", retrievedUserRole.getRoleName());
         assertEquals("Joe", retrievedUserRole.getUser().getFirstName());
     }
@@ -69,15 +70,14 @@ class RoleDaoTest {
 
     @Test
     void deleteSuccess() {
-        GenericDao userDao = new GenericDao(User.class);
-        User testUser = (User)userDao.getById(1);
+        User testUser = getTestUserWithIdOf1();
         UserRole testUserRole = new UserRole("unitTester", testUser);
         testUser.addRole(testUserRole);
-        genericDao.insert(testUserRole);
-        List<UserRole> userRoles = genericDao.getAll();
+        userRoleDao.insert(testUserRole);
+        List<UserRole> userRoles = userRoleDao.getAll();
         assertEquals(7, userRoles.size());
-        genericDao.delete(testUserRole);
-        userRoles = genericDao.getAll();
+        userRoleDao.delete(testUserRole);
+        userRoles = userRoleDao.getAll();
         assertEquals(6, userRoles.size());
     }
 
@@ -85,7 +85,7 @@ class RoleDaoTest {
     void findByAdmin() {
         String searchTerm = "admin";
         String searchProperty = "roleName";
-        List results = genericDao.findByPropertyEqual(searchProperty, searchTerm);
+        List results = userRoleDao.findByPropertyEqual(searchProperty, searchTerm);
         assertEquals(results.size(), 2);
 
         UserRole resultOne = (UserRole)results.get(0);
