0e1c9fce8d3399cf9cdcc04a15d24d05befc3dd1
RobIsInSchool <radams8@madisoncollege.edu>
2021-02-27 10:43:54 -0600

    refactored genericDao to userDao

--- src/test/java/com/radams/persistence/UserDaoTest.java
+++ src/test/java/com/radams/persistence/UserDaoTest.java
@@ -8,32 +8,31 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoTest {
 
-    GenericDao genericDao;
+    GenericDao userDao;
 
     @BeforeEach
     void setUp() {
-        genericDao = new GenericDao(User.class);
+        userDao = new GenericDao(User.class);
         Database db = Database.getInstance();
         db.runSQL("cleandb.sql");
     }
 
     @Test
     void getAllUsersSuccess() {
-        List<User> users = genericDao.getAll();
+        List<User> users = userDao.getAll();
         assertEquals(6, users.size());
     }
 
 
     @Test
     void getByIdSuccess() {
-        User retrievedUser = (User)genericDao.getById(2);
+        User retrievedUser = (User) userDao.getById(2);
         assertNotNull(retrievedUser);
         assertEquals("Hensen", retrievedUser.getLastName());
     }
@@ -41,10 +40,10 @@ class UserDaoTest {
     @Test
     void saveOrUpdateSuccess() {
         String newLastName = "changed";
-        User userToUpdate = (User)genericDao.getById(1);
+        User userToUpdate = (User) userDao.getById(1);
         userToUpdate.setLastName(newLastName);
-        genericDao.saveOrUpdate(userToUpdate);
-        User retrievedUser = (User)genericDao.getById(1);
+        userDao.saveOrUpdate(userToUpdate);
+        User retrievedUser = (User) userDao.getById(1);
         assertEquals(retrievedUser.getLastName(), newLastName);
     }
 
@@ -52,8 +51,8 @@ class UserDaoTest {
     @Test
     void insertSuccess() {
         User testUser = new User("test", "user", "testuser", "email", "password", true, Date.valueOf(LocalDate.now()));
-        int newId = genericDao.insert(testUser);
-        User retrievedUser = (User)genericDao.getById(newId);
+        int newId = userDao.insert(testUser);
+        User retrievedUser = (User) userDao.getById(newId);
         assertEquals(newId, retrievedUser.getUserId());
     }
 
@@ -62,8 +61,8 @@ class UserDaoTest {
         User testUser = new User("test", "user", "testuser", "email", "password", true, Date.valueOf(LocalDate.now()));
         UserRole role = new UserRole("testRole", testUser);
         testUser.addRole(role);
-        int id = genericDao.insert(testUser);
-        User retrievedUser = (User)genericDao.getById(id);
+        int id = userDao.insert(testUser);
+        User retrievedUser = (User) userDao.getById(id);
         assertEquals(id, retrievedUser.getUserId());
         assertEquals(1, retrievedUser.getUserRoles().size());
     }
@@ -71,11 +70,11 @@ class UserDaoTest {
     @Test
     void deleteSuccess() {
         User testUser = new User("test", "user", "testuser", "email", "password", true, Date.valueOf(LocalDate.now()));
-        genericDao.insert(testUser);
-        List<User> users = genericDao.getAll();
+        userDao.insert(testUser);
+        List<User> users = userDao.getAll();
         assertEquals(7, users.size());
-        genericDao.delete(testUser);
-        users = genericDao.getAll();
+        userDao.delete(testUser);
+        users = userDao.getAll();
         assertEquals(6, users.size());
     }
 
@@ -83,7 +82,7 @@ class UserDaoTest {
     void findByLastNamePropertySuccess() throws Exception {
          String searchTerm = "Coyne";
          String searchProperty = "lastName";
-         List results = genericDao.findByPropertyEqual(searchProperty, searchTerm);
+         List results = userDao.findByPropertyEqual(searchProperty, searchTerm);
          assertTrue(results.size() > 0);
          User result = (User)results.get(0);
          assertEquals(result.getLastName(), searchTerm);
