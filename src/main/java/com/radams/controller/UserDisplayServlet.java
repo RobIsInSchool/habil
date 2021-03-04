ec397a38f1986b9147d75dea00806270b7b66d39
RobIsInSchool <radams8@madisoncollege.edu>
2021-03-04 11:43:54 -0600

    temporary servlet to simply display all users

--- src/main/java/com/radams/controller/UserDisplayServlet.java
+++ src/main/java/com/radams/controller/UserDisplayServlet.java
@@ -1,18 +1,24 @@
 package com.radams.controller;
 
 import com.radams.entity.User;
+import com.radams.persistence.GenericDao;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
-@WebServlet(name = "UserDisplayServlet", value = "/UserDisplayServlet")
+@WebServlet(name = "UserDisplayServlet", value = "/UserDisplay")
 public class UserDisplayServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ArrayList<User> users = new ArrayList<User>();
+        GenericDao<User> userDao = new GenericDao(User.class);
+        List<User> users = userDao.getAll();
+        request.setAttribute("users", users);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/displayUsers.jsp");
+        dispatcher.forward(request, response);
     }
 
     @Override
