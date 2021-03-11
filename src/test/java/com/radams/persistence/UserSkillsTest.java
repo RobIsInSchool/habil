9df5aaf74a6d11b40e1b6edb5e2486c815bb0c57
RobIsInSchool <radams8@madisoncollege.edu>
2021-03-11 10:36:23 -0600

    attempting to successfully match users

--- src/test/java/com/radams/persistence/UserSkillsTest.java
+++ src/test/java/com/radams/persistence/UserSkillsTest.java
@@ -9,10 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -91,5 +88,60 @@ public class UserSkillsTest {
         User updatedJoeCoyneWithout = (User)userDao.getById(1);
         assertEquals(0, updatedJoeCoyneWithout.getSkillsWants().size());
 
+    }
+
+    @Test
+    void matchUsersBasedOnSkillsSuccess() {
+        User joeCoyne = (User)userDao.getById(1);
+        User karenMack = (User)userDao.getById(4);
+        User barneyCurry = (User)userDao.getById(3);
+        Skill autoMaintenance = (Skill)skillDao.getById(2);
+        Skill fishing = (Skill)skillDao.getById(1);
+
+        //Joe Coyne wants to learn auto maintenance - Karen Mack and Barney Curry know auto maintenance
+        //Joe Coyne knows how to fish, but only Karen Mack wants to learn how to fish
+        joeCoyne.addSkillsWants(autoMaintenance);
+        joeCoyne.addSkillHas(fishing);
+        karenMack.addSkillHas(autoMaintenance);
+        karenMack.addSkillsWants(fishing);
+        barneyCurry.addSkillHas(autoMaintenance);
+
+        userDao.saveOrUpdate(joeCoyne);
+        userDao.saveOrUpdate(karenMack);
+        userDao.saveOrUpdate(barneyCurry);
+
+        List<User> otherUsers = userDao.getAll();
+//        otherUsers.remove(joeCoyne);
+        Set<User> matchedUsers = new HashSet<>();
+
+        Set<Skill> joeSkillsWants = joeCoyne.getSkillsWants();
+        Set<Skill> joeSkillsHas = joeCoyne.getSkillsHas();
+
+        for (User user : otherUsers) {
+            Set<Skill> userWants = user.getSkillsWants();
+            Set<Skill> userHas = user.getSkillsHas();
+
+            Set<Skill> allSkills = new HashSet<>();
+            allSkills.addAll(userWants);
+            allSkills.addAll(userHas);
+
+            for (Skill skill : allSkills) {
+                int skillId = skill.getSkillId();
+                for (Skill wantSkill : joeSkillsWants) {
+                    int wantSkillId = wantSkill.getSkillId();
+                    if(wantSkillId == skillId) {
+                        System.out.println("joe wants auto");
+                        for (Skill joeSkillHas : joeSkillsHas) {
+                            int hasSkillId = joeSkillHas.getSkillId();
+                            System.out.println("Joe has: " + joeSkillHas);
+
+                            if (userHas.contains(skill) && userWants.contains(joeSkillHas)) {
+                                matchedUsers.add(user);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
