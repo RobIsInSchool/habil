e4e5779ed928eae4dd290ddf99fc67680f520f56
RobIsInSchool <radams8@madisoncollege.edu>
2021-03-21 13:12:22 -0500

    trying to figure out how to get user info in servlet

--- src/main/java/com/radams/controller/DisplayMatchedUsersServlet.java
+++ src/main/java/com/radams/controller/DisplayMatchedUsersServlet.java
@@ -4,12 +4,18 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.Collections;
 
-@WebServlet(name = "DisplayMatchedUsersServlet", value = "/DisplayMatchedUsersServlet")
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@WebServlet(name = "DisplayMatchedUsersServlet", value = "/viewMatches")
 public class DisplayMatchedUsersServlet extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        ServletContext context = getServletContext();
+        logger.info(context.getAttribute("user_name"));
     }
 
     @Override
