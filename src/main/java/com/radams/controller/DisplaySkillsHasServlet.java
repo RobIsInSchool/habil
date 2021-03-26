6e050809decc6e05ffaa45c280c1692118465f4c
RobIsInSchool <radams8@madisoncollege.edu>
2021-03-26 13:56:14 -0500

    users can now view their skills-has

---
+++ src/main/java/com/radams/controller/DisplaySkillsHasServlet.java
@@ -0,0 +1,35 @@
+package com.radams.controller;
+
+import com.radams.entity.Skill;
+import com.radams.entity.User;
+import com.radams.persistence.GenericDao;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet(name = "DisplaySkillsHasServlet", value = "/skillsHas")
+public class DisplaySkillsHasServlet extends HttpServlet {
+    private GenericDao userDao = new GenericDao(User.class);
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        Set<Skill> skillsHas = user.getSkillsHas();
+        session.setAttribute("skillsHas", skillsHas);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/skillsHas.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/skillsHas.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: student
+  Date: 3/4/21
+  Time: 10:24 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html lang="en">
+<c:import var="head" url="components/head.jsp" />
+<c:import var="nav" url="components/navigation.jsp" />
+<c:out value="${head}" escapeXml="false"/>
+<body>
+    <div class="container">
+    <header>
+        <c:out value="${nav}" escapeXml="false"/>
+    </header>
+    <p><a href="#">Add New Skill</a></p>
+    <ul>
+    <c:forEach var="skill" items="${skillsHas}">
+        <li>${skill.skillName}<form><input type="hidden" name="${skill.skillId}"><input type="submit" value="Remove Skill?"></form></li>
+    </c:forEach>
+    </ul>
+    </div>
+</body>
+</html>
