67ae5c198a1fc0201d1212628bbfe2b7cf8e8f98
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-20 13:22:06 -0500

    added necessary imports and instance vars

--- src/main/java/com/radams/controller/DeleteSkillsServlet.java
+++ src/main/java/com/radams/controller/DeleteSkillsServlet.java
@@ -5,8 +5,17 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
+import java.util.List;
+
+import com.radams.entity.Skill;
+import com.radams.persistence.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @WebServlet(name = "DeleteSkillsServlet", value = "/DeleteSkillsServlet")
 public class DeleteSkillsServlet extends HttpServlet {
+    private GenericDao skillDao = new GenericDao(Skill.class);
+    private final Logger logger = LogManager.getLogger(this.getClass());
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
