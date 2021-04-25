56a9368dcb77cf7db4fed580a5a97265e61bf9db
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-25 13:49:02 -0500

    added servlet to redirect home

---
+++ src/main/java/com/radams/controller/GoHomeServlet.java
@@ -0,0 +1,20 @@
+package com.radams.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "GoHomeServlet", value = "/home")
+public class GoHomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/home.jsp");
+        dispatcher.forward(request, response);
+    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+}
