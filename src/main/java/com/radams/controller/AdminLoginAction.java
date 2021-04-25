b025bcad5d5f0513499b35a3d1b01ad131bd3485
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-25 14:41:27 -0500

    added jdoc, spacing for readability, removed unused doPost

--- src/main/java/com/radams/controller/AdminLoginAction.java
+++ src/main/java/com/radams/controller/AdminLoginAction.java
@@ -13,31 +13,39 @@ import com.radams.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
+/**
+ * Logs in Admin
+ * @author Robert Adams
+ */
 @WebServlet(name = "AdminLoginAction", value = "/adminLoginAction")
 public class AdminLoginAction extends HttpServlet {
     private GenericDao userDao = new GenericDao(User.class);
     private GenericDao skillDao = new GenericDao(Skill.class);
     private final Logger logger = LogManager.getLogger(this.getClass());
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         HttpSession session = request.getSession();
+
         String username = request.getRemoteUser();
         logger.info("Admin " + username + " has logged in");
         List<User> foundUser = (List<User>) userDao.findByPropertyEqual("username", username);
         User user = foundUser.get(0);
+
         List<User> allUsers = (List<User>) userDao.getAll();
         List<Skill> allSkills = (List<Skill>) skillDao.getAll();
+
         request.setAttribute("username", username);
         request.setAttribute("user", user);
+
         session.setAttribute("user", user);
         session.setAttribute("allUsers", allUsers);
         session.setAttribute("allSkills", allSkills);
+
         RequestDispatcher dispatcher = request.getRequestDispatcher("/admin.jsp");
         dispatcher.forward(request, response);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
