ade5555f75b0a89a30884bed2f08ee12c75ab5d2
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-25 15:00:30 -0500

    added missing jdocs

--- src/main/java/com/radams/entity/Skill.java
+++ src/main/java/com/radams/entity/Skill.java
@@ -98,14 +98,26 @@ public class Skill {
         this.skillName = skillName;
     }
 
+    /**
+     * Add users who have skill
+     * @param user the user
+     */
     public void addUsersHas(User user) {
         usersHave.add(user);
     }
 
+    /**
+     * remove users who have skill
+     * @param user the user
+     */
     public void removeUsersHas(User user) {
         usersHave.remove(user);
     }
 
+    /**
+     * Add users who want skill
+     * @param user the user
+     */
     public void addUsersWants(User user) {
         usersWant.add(user);
     }
@@ -119,6 +131,12 @@ public class Skill {
     }
 
     //Muchas gracias a https://medium.com/codelog/overriding-hashcode-method-effective-java-notes-723c1fedf51c para su ayuda
+
+    /**
+     * Overrides equals
+     * @param obj the object
+     * @return boolean indicating equivalence
+     */
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Skill)) {
