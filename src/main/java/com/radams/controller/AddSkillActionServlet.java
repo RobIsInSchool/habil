0a380863febb651da072865ddb7afe675e119e83
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-25 14:29:36 -0500

    added jdoc, line spacing for readability, minor refactor for clarity

--- src/main/java/com/radams/controller/AddSkillActionServlet.java
+++ src/main/java/com/radams/controller/AddSkillActionServlet.java
@@ -11,6 +11,10 @@ import com.radams.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * This servlet adds a user skill based on type
+ * @author Robert Adams
+ */
 @WebServlet(name = "AddSkillActionServlet", value = "/addSkillAction")
 public class AddSkillActionServlet extends HttpServlet {
 
@@ -20,25 +24,32 @@ public class AddSkillActionServlet extends HttpServlet {
     //TODO condense this and remove skill servlet into ONE servlet
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         HttpSession webSession = request.getSession();
+
+        //"webUser" and "user" are used to distinguish between the session user and the same user in the database
         User webUser = (User) webSession.getAttribute("user");
         User user = (User) userDao.getById(webUser.getUserId());
+
         String skillType = request.getParameter("skillType");
         int skillId = Integer.parseInt(request.getParameter("skillId"));
         Skill skill = (Skill) skillDao.getById(skillId);
+
         logger.info("Skill type: " + skillType + " being added for user " + user.getUsername());
         logger.info("said skill is: " + skill.getSkillName());
-        String forwardUrl = "";
+
+        String redirectURL = "";
+        //determine skill type and redirect accordingly
         if (skillType.equals("has")) {
-            forwardUrl = "/skillsHas";
+            redirectURL = "/skillsHas";
             user.addSkillHas(skill);
         }
         if (skillType.equals("wants")) {
-            forwardUrl = "/skillsWants";
+            redirectURL = "/skillsWants";
             user.addSkillWants(skill);
         }
         userDao.saveOrUpdate(user);
         webSession.setAttribute("user", user);
-        response.sendRedirect(request.getContextPath() + forwardUrl);
+        response.sendRedirect(request.getContextPath() + redirectURL);
     }
 }
