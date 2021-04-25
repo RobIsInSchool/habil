f682f5b4e50f92eb394bead2bc65731018e8e391
RobIsInSchool <radams8@madisoncollege.edu>
2021-04-25 15:18:46 -0500

    removed unused imports

--- src/main/java/com/radams/persistence/GenericDao.java
+++ src/main/java/com/radams/persistence/GenericDao.java
@@ -1,10 +1,8 @@
 package com.radams.persistence;
 
-import com.radams.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import javax.persistence.criteria.*;
