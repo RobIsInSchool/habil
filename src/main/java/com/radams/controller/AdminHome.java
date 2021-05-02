ef7779ce3ff7512c3f950296ced17c04f8745c63
RobIsInSchool <radams8@madisoncollege.edu>
2021-05-02 17:33:31 -0500

    servlet rename

--- src/main/java/com/radams/controller/AdminHome.java
+++ src/main/java/com/radams/controller/AdminHome.java
@@ -9,17 +9,18 @@ import java.util.List;
 
 import com.radams.entity.Skill;
 import com.radams.entity.User;
+import com.radams.entity.UserRole;
 import com.radams.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
 /**
- * Logs in Admin
+ * Sets admin homepage
  * @author Robert Adams
  */
-@WebServlet(name = "AdminLoginAction", value = "/adminLoginAction")
-public class AdminLoginAction extends HttpServlet {
+@WebServlet(name = "AdminLoginAction", value = "/adminHome")
+public class AdminHome extends HttpServlet {
     private GenericDao userDao = new GenericDao(User.class);
     private GenericDao skillDao = new GenericDao(Skill.class);
     private final Logger logger = LogManager.getLogger(this.getClass());
@@ -30,7 +31,7 @@ public class AdminLoginAction extends HttpServlet {
         HttpSession session = request.getSession();
 
         String username = request.getRemoteUser();
-        logger.info("Admin " + username + " has logged in");
+
         List<User> foundUser = (List<User>) userDao.findByPropertyEqual("username", username);
         User user = foundUser.get(0);
 
@@ -43,6 +44,13 @@ public class AdminLoginAction extends HttpServlet {
         session.setAttribute("user", user);
         session.setAttribute("allUsers", allUsers);
         session.setAttribute("allSkills", allSkills);
+        boolean isOwner = false;
+        for(UserRole role : user.getUserRoles()) {
+            if (role.getRoleName().equals("owner")) {
+                isOwner = true;
+            }
+        }
+        session.setAttribute("isOwner", isOwner);
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/admin.jsp");
         dispatcher.forward(request, response);
