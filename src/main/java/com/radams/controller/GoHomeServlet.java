c3384a47cec0b444f10257f7e59ffdc2d8547dbc
RobIsInSchool <radams8@madisoncollege.edu>
2021-05-02 12:01:05 -0500

    user's lesson ratio gets calculated every time they visit the homepage

--- src/main/java/com/radams/controller/GoHomeServlet.java
+++ src/main/java/com/radams/controller/GoHomeServlet.java
@@ -1,9 +1,14 @@
 package com.radams.controller;
 
+import com.radams.entity.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.lang.*;
 
 /**
  * Sends the user home
@@ -13,6 +18,25 @@ import java.io.IOException;
 public class GoHomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        int lessonsTaken = user.getNumLessonsTaken();
+        int lessonsTaught = user.getNumLessonsTaught();
+        Double lessonRatio;
+        if (lessonsTaken == 0) {
+            lessonRatio = 0.0;
+        } else {
+            lessonRatio =
+                    new BigDecimal((double) lessonsTaught / lessonsTaken)
+                            .round(new MathContext(2))
+                            .doubleValue();
+
+        }
+
+        request.setAttribute("numTaken", lessonsTaken);
+        request.setAttribute("numTaught", lessonsTaught);
+        request.setAttribute("lessonRatio", lessonRatio);
+
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/home.jsp");
         dispatcher.forward(request, response);
     }
