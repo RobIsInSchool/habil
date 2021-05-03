35fa843b63fdc0dfc3dc0898886e4dc9ff404bd8
RobIsInSchool <radams8@madisoncollege.edu>
2021-05-03 10:53:23 -0500

    implemented proper password hashing

--- src/main/java/com/radams/controller/SignupUserServlet.java
+++ src/main/java/com/radams/controller/SignupUserServlet.java
@@ -3,10 +3,12 @@ package com.radams.controller;
 import com.radams.entity.User;
 import com.radams.entity.UserRole;
 import com.radams.persistence.GenericDao;
-
+import org.apache.catalina.CredentialHandler;
+import org.apache.catalina.Globals;
+import org.apache.catalina.realm.MessageDigestCredentialHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+import java.security.NoSuchAlgorithmException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -29,13 +31,21 @@ public class SignupUserServlet extends HttpServlet {
 
         HttpSession session = request.getSession();
 
+        MessageDigestCredentialHandler credentialHandler = new MessageDigestCredentialHandler();
+        try {
+            credentialHandler.setAlgorithm("sha-256");
+        } catch (NoSuchAlgorithmException e) {
+            logger.info("unable to implement hash algorithm");
+        }
+        credentialHandler.setEncoding("UTF-8");
+
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
         String userName = request.getParameter("username");
         String email = request.getParameter("email");
-        String password = request.getParameter("password");
+        String password = credentialHandler.mutate(request.getParameter("password"));
         String zip = request.getParameter("zip");
-
+        logger.info("Hashed password length: " + password.length());
         User user = new User(firstName, lastName, userName, email, password, true, zip, Date.valueOf(LocalDate.now()));
         UserRole role = new UserRole("regUser", user);
         user.addRole(role);
